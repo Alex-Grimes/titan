d7c145135def2a21e2e321228b82d7094e8c0292
Alex Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2025-03-06 14:37:04 -0500

    Update invoice.ts

--- titan-ui/src/models/Invoice/invoice.ts
+++ titan-ui/src/models/Invoice/invoice.ts
@@ -1,11 +1,11 @@
-export default class Invoice {
+export class Invoice {
   id: string
   billerStreetAddress: string
   billerCity: string
   billerZip: string
 
-  public constructor(invoice: Invoice) {
-    this.id = invoice.id
+constructor(opts?: Partial<Invoice>) {
+    this.id = opts.id
     this.billerStreetAddress = invoice.billerStreetAddress
     this.billerCity = invoice.billerCity
     this.billerZip = invoice.billerZip
